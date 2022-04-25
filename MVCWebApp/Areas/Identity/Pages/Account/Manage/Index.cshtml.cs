4266b649e12a3bbae7a804939f4f0ef787595383
Mattias Gustafsson <mattiasgu87@gmail.com>
2022-04-25 22:33:43 +0200

    small bug change

--- MVCWebApp/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ MVCWebApp/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -51,9 +51,6 @@ namespace MVCWebApp.Areas.Identity.Pages.Account.Manage
             [Display(Name = "Last Name")]
             public string LastName { get; set; }
 
-            [Required]
-            [RegularExpression(@"(^((((0[1-9])|([1-2][0-9])|(3[0-1]))|([1-9]))\x2F(((0[1-9])|(1[0-2]))|([1-9]))\x2F(([0-9]{2})|(((19)|([2]([0]{1})))([0-9]{2}))))$)",
-            ErrorMessage = "Please enter a valid birth date! example: dd/mm/yyyy")]
             [Display(Name = "Birth Date")]
             public string BirthDate { get; set; }
         }
@@ -108,10 +105,11 @@ namespace MVCWebApp.Areas.Identity.Pages.Account.Manage
                 }
             }
 
-            //custom ApplicationUser fields-> todo-> make safer
+            //custom ApplicationUser fields-> todo-> make safer?
             user.FirstName = Input.FirstName;
             user.LastName = Input.LastName;
             _context.Users.Update(user);
+            await _context.SaveChangesAsync();
 
             await _signInManager.RefreshSignInAsync(user);
             StatusMessage = "Your profile has been updated";
